b1139a5a3513ca914371aa388c89d9fff03b0192
hanskoder963 <hhm042025@gmail.com>
2025-09-22 12:15:08 +0200

    stufs

---
+++ routes/refresh.js
@@ -0,0 +1,8 @@
+//imports
+const express = require("express");
+const router = express.Router();
+const refreshController = require("../controller/refreshController");
+
+router.post("/", refreshController.handleRefreshToken);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 // IMPORTS
-
+//third party imports
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
+//first party imports
 const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
 const corsOptions = require("./config/corsOptions");
+
+const verifyJWT = require("./middleware/verifyJWT");
+
 //port definition
 const PORT = process.env.PORT || 3500;
 const app = express();
@@ -17,16 +22,21 @@ app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(errorHandler);
-
+//routes
 app.use("/", require("./routes/root"));
 app.use("/subdir", require("./routes/subdir/subdir"));
-app.use("/employees", require("./routes/api/employees"));
 app.use("/register", require("./routes/register"));
 app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
+
+//beskyttet
+app.use(verifyJWT);
+app.use("/employees", require("./routes/api/employees"));
 
 app.all(/.*/, (req, res) => {
   res.status(404);
